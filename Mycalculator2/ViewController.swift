420da10614fe26b7e3348d463fe3d2fc338f8e93
is-9108 <syouta103010@gmail.com>
2020-09-03 21:16:53 +0900

    Initial Commit

---
+++ Mycalculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mycalculator2
+//
+//  Created by Shota Ishii on 2020/09/03.
+//  Copyright © 2020 is. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
